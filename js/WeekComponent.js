9b371677f8ea58fc61c85195e975f345d3416ab5
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-10-12 09:18:43 -0300

    Se modifica la invocacion de los eventos. Falta arreglar bug en invocacion.

--- js/WeekComponent.js
+++ js/WeekComponent.js
@@ -161,13 +161,13 @@ async function eventsWeek() {
                 let fechaEvento = new Date(datesSplit[0], datesSplit[1] - 1, datesSplit[2] , hoursSplit[0], hoursSplit[1], hoursSplit[2]);
                 let weekContent = document.getElementById("days-of-week");
                 let cont = 1;
-                let day = 1; //El dia de la semana L a D |
-                let horas = 0; //La hora del evento |
+                let day = 1; //El dia de la semana L a D | Actual en el calendario
+                let horas = 0; //La hora del evento | Actual en el calendario
                 let tmpEventDayWeek = fechaEvento.getDay(); // Trae el dia de la semana del json
                 let tmpHourDayWeek = fechaEvento.getHours(); // Trae la hora del json
 
                 for (let w = 1; w < 169; w++) {
-                    if(horas === tmpHourDayWeek && tmpEventDayWeek === day && fechaEvento.getMonth() === currentMonth && fechaEvento.getFullYear() === currentYear){
+                    if(horas === tmpHourDayWeek /* && tmpEventDayWeek === day */ && fechaEvento.getDate() === currentWeek && fechaEvento.getMonth() === currentMonth && fechaEvento.getFullYear() === currentYear){
                         eventWekk[w].innerHTML += 
                             `<button id="event-Modal" class="btn-item" onclick="handleBtn()"><span class="sp-hour"> ${hoursJSON} </span> - <span class="sp-title"> ${titleEvent} </span></button>`;
                     }
