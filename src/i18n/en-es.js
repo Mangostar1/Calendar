3b857b3e9c017243512d5e28ec551e872d501aee
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-05-13 21:38:42 -0400

    Se agrega nuevo archivo para manejar el idioma de forma aislada al resto del codigo.

---
+++ src/i18n/en-es.js
@@ -0,0 +1,45 @@
+let languageHandler = {
+  es: {
+    prep: ["de", "del"],
+  },
+  en: {
+    prep: ["in", "of"],
+  },
+};
+
+//! Agregar este array dentro del handler para su respectivo manejo entre el ingles y el español.
+const nameMonthEs = [
+  //<-- Usend on some functions.
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+const nameDayEs = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+];
+
+const daysOfWeekEs = [
+  { day: "Lun" },
+  { day: "Mar" },
+  { day: "Mié" },
+  { day: "Jue" },
+  { day: "Vie" },
+  { day: "Sáb" },
+  { day: "Dom" },
+];
