e827b0b88ed9d915ddf4c83353591abe6d0dfd03
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-05-15 15:38:18 -0400

    Se comenta en que lugares se esta haciendo uso del objeto del en-es.js

--- src/i18n/en-es.js
+++ src/i18n/en-es.js
@@ -1,3 +1,20 @@
+/* 
+  This object controls the language in the entire app. If you need to change it, you just need to replace 'languageHandler.es./' with 'languageHandler.en./'.
+
+  This object is used in:
+
+    index.js --> line: 108, 109, 194, 195, 196, 211, 212, 222, 223, 264. 265. 266, 291, 292, 293, 312, 313, 329, 330, 357, 358.
+
+    DayComponent.js --> line: 70
+
+    WeekComponent.js --> line: 12
+
+    MonthComponent.js --> line: 32
+
+    YearComponent.js --> line: 12 (don't change line 14 and 15)
+
+*/
+
 const languageHandler = {
   es: {
     prep: ["de", "del"],
